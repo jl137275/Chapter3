4915696ad517d8bb9374a5caa00e2d024cb63e92
jl137275 <33095838+jl137275@users.noreply.github.com>
2017-11-08 10:30:58 -0600

    Finished Format Demo

---
+++ Format Demo/Format Demo/Form1.Designer.vb
@@ -0,0 +1,218 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblNumberOrDate = New System.Windows.Forms.Label()
+        Me.lblFormatted = New System.Windows.Forms.Label()
+        Me.lblFormatResult = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.btnNumberFormat = New System.Windows.Forms.Button()
+        Me.btnFixedPointFormat = New System.Windows.Forms.Button()
+        Me.btnExponentialFormat = New System.Windows.Forms.Button()
+        Me.btnCurrencyFormat = New System.Windows.Forms.Button()
+        Me.btnPercentFormat = New System.Windows.Forms.Button()
+        Me.btnShortDate = New System.Windows.Forms.Button()
+        Me.btnLongDate = New System.Windows.Forms.Button()
+        Me.btnShortTime = New System.Windows.Forms.Button()
+        Me.btnLongTime = New System.Windows.Forms.Button()
+        Me.btnFullDateTime = New System.Windows.Forms.Button()
+        Me.txtUserInput = New System.Windows.Forms.TextBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'lblNumberOrDate
+        '
+        Me.lblNumberOrDate.AutoSize = True
+        Me.lblNumberOrDate.Location = New System.Drawing.Point(12, 52)
+        Me.lblNumberOrDate.Name = "lblNumberOrDate"
+        Me.lblNumberOrDate.Size = New System.Drawing.Size(115, 13)
+        Me.lblNumberOrDate.TabIndex = 0
+        Me.lblNumberOrDate.Text = "Enter a number or date"
+        '
+        'lblFormatted
+        '
+        Me.lblFormatted.AutoSize = True
+        Me.lblFormatted.Location = New System.Drawing.Point(38, 108)
+        Me.lblFormatted.Name = "lblFormatted"
+        Me.lblFormatted.Size = New System.Drawing.Size(54, 13)
+        Me.lblFormatted.TabIndex = 1
+        Me.lblFormatted.Text = "Formatted"
+        '
+        'lblFormatResult
+        '
+        Me.lblFormatResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblFormatResult.Location = New System.Drawing.Point(240, 108)
+        Me.lblFormatResult.Name = "lblFormatResult"
+        Me.lblFormatResult.Size = New System.Drawing.Size(212, 24)
+        Me.lblFormatResult.TabIndex = 2
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.btnFullDateTime)
+        Me.GroupBox1.Controls.Add(Me.btnFixedPointFormat)
+        Me.GroupBox1.Controls.Add(Me.btnLongTime)
+        Me.GroupBox1.Controls.Add(Me.btnNumberFormat)
+        Me.GroupBox1.Controls.Add(Me.btnShortTime)
+        Me.GroupBox1.Controls.Add(Me.btnExponentialFormat)
+        Me.GroupBox1.Controls.Add(Me.btnLongDate)
+        Me.GroupBox1.Controls.Add(Me.btnPercentFormat)
+        Me.GroupBox1.Controls.Add(Me.btnShortDate)
+        Me.GroupBox1.Controls.Add(Me.btnCurrencyFormat)
+        Me.GroupBox1.Location = New System.Drawing.Point(41, 183)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(390, 196)
+        Me.GroupBox1.TabIndex = 3
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Select a Format"
+        '
+        'btnNumberFormat
+        '
+        Me.btnNumberFormat.Location = New System.Drawing.Point(11, 27)
+        Me.btnNumberFormat.Name = "btnNumberFormat"
+        Me.btnNumberFormat.Size = New System.Drawing.Size(129, 23)
+        Me.btnNumberFormat.TabIndex = 4
+        Me.btnNumberFormat.Text = "Number format (n)"
+        Me.btnNumberFormat.UseVisualStyleBackColor = True
+        '
+        'btnFixedPointFormat
+        '
+        Me.btnFixedPointFormat.Location = New System.Drawing.Point(11, 56)
+        Me.btnFixedPointFormat.Name = "btnFixedPointFormat"
+        Me.btnFixedPointFormat.Size = New System.Drawing.Size(129, 23)
+        Me.btnFixedPointFormat.TabIndex = 5
+        Me.btnFixedPointFormat.Text = "Fixed-point format (f)"
+        Me.btnFixedPointFormat.UseVisualStyleBackColor = True
+        '
+        'btnExponentialFormat
+        '
+        Me.btnExponentialFormat.Location = New System.Drawing.Point(11, 85)
+        Me.btnExponentialFormat.Name = "btnExponentialFormat"
+        Me.btnExponentialFormat.Size = New System.Drawing.Size(129, 23)
+        Me.btnExponentialFormat.TabIndex = 6
+        Me.btnExponentialFormat.Text = "Exponential format (e)"
+        Me.btnExponentialFormat.UseVisualStyleBackColor = True
+        '
+        'btnCurrencyFormat
+        '
+        Me.btnCurrencyFormat.Location = New System.Drawing.Point(11, 114)
+        Me.btnCurrencyFormat.Name = "btnCurrencyFormat"
+        Me.btnCurrencyFormat.Size = New System.Drawing.Size(129, 23)
+        Me.btnCurrencyFormat.TabIndex = 7
+        Me.btnCurrencyFormat.Text = "Currency format (c)"
+        Me.btnCurrencyFormat.UseVisualStyleBackColor = True
+        '
+        'btnPercentFormat
+        '
+        Me.btnPercentFormat.Location = New System.Drawing.Point(11, 143)
+        Me.btnPercentFormat.Name = "btnPercentFormat"
+        Me.btnPercentFormat.Size = New System.Drawing.Size(129, 23)
+        Me.btnPercentFormat.TabIndex = 8
+        Me.btnPercentFormat.Text = "Percent format (p)"
+        Me.btnPercentFormat.UseVisualStyleBackColor = True
+        '
+        'btnShortDate
+        '
+        Me.btnShortDate.Location = New System.Drawing.Point(199, 27)
+        Me.btnShortDate.Name = "btnShortDate"
+        Me.btnShortDate.Size = New System.Drawing.Size(100, 23)
+        Me.btnShortDate.TabIndex = 9
+        Me.btnShortDate.Text = "Short Date (d)"
+        Me.btnShortDate.UseVisualStyleBackColor = True
+        '
+        'btnLongDate
+        '
+        Me.btnLongDate.Location = New System.Drawing.Point(199, 56)
+        Me.btnLongDate.Name = "btnLongDate"
+        Me.btnLongDate.Size = New System.Drawing.Size(100, 23)
+        Me.btnLongDate.TabIndex = 10
+        Me.btnLongDate.Text = "Long Date (D)"
+        Me.btnLongDate.UseVisualStyleBackColor = True
+        '
+        'btnShortTime
+        '
+        Me.btnShortTime.Location = New System.Drawing.Point(199, 85)
+        Me.btnShortTime.Name = "btnShortTime"
+        Me.btnShortTime.Size = New System.Drawing.Size(100, 23)
+        Me.btnShortTime.TabIndex = 11
+        Me.btnShortTime.Text = "Short Time (t)"
+        Me.btnShortTime.UseVisualStyleBackColor = True
+        '
+        'btnLongTime
+        '
+        Me.btnLongTime.Location = New System.Drawing.Point(199, 114)
+        Me.btnLongTime.Name = "btnLongTime"
+        Me.btnLongTime.Size = New System.Drawing.Size(100, 23)
+        Me.btnLongTime.TabIndex = 12
+        Me.btnLongTime.Text = "Long Time (T)"
+        Me.btnLongTime.UseVisualStyleBackColor = True
+        '
+        'btnFullDateTime
+        '
+        Me.btnFullDateTime.Location = New System.Drawing.Point(199, 143)
+        Me.btnFullDateTime.Name = "btnFullDateTime"
+        Me.btnFullDateTime.Size = New System.Drawing.Size(100, 23)
+        Me.btnFullDateTime.TabIndex = 13
+        Me.btnFullDateTime.Text = "Full Date/Time (F)"
+        Me.btnFullDateTime.UseVisualStyleBackColor = True
+        '
+        'txtUserInput
+        '
+        Me.txtUserInput.Location = New System.Drawing.Point(240, 45)
+        Me.txtUserInput.Name = "txtUserInput"
+        Me.txtUserInput.Size = New System.Drawing.Size(100, 20)
+        Me.txtUserInput.TabIndex = 4
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(464, 448)
+        Me.Controls.Add(Me.txtUserInput)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.lblFormatResult)
+        Me.Controls.Add(Me.lblFormatted)
+        Me.Controls.Add(Me.lblNumberOrDate)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblNumberOrDate As System.Windows.Forms.Label
+    Friend WithEvents lblFormatted As System.Windows.Forms.Label
+    Friend WithEvents lblFormatResult As System.Windows.Forms.Label
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents btnFixedPointFormat As System.Windows.Forms.Button
+    Friend WithEvents btnNumberFormat As System.Windows.Forms.Button
+    Friend WithEvents btnExponentialFormat As System.Windows.Forms.Button
+    Friend WithEvents btnPercentFormat As System.Windows.Forms.Button
+    Friend WithEvents btnShortDate As System.Windows.Forms.Button
+    Friend WithEvents btnCurrencyFormat As System.Windows.Forms.Button
+    Friend WithEvents btnLongDate As System.Windows.Forms.Button
+    Friend WithEvents btnShortTime As System.Windows.Forms.Button
+    Friend WithEvents btnLongTime As System.Windows.Forms.Button
+    Friend WithEvents btnFullDateTime As System.Windows.Forms.Button
+    Friend WithEvents txtUserInput As System.Windows.Forms.TextBox
+
+End Class
---
+++ Format Demo/Format Demo/Form1.vb
@@ -0,0 +1,114 @@
+﻿Public Class Form1
+
+
+
+    Private Sub btnNumberFormat_Click(sender As Object, e As EventArgs) Handles btnNumberFormat.Click
+        Dim dblAnswer As Double
+        Dim strResult As String
+
+        dblAnswer = CDbl(txtUserInput.Text)
+
+
+        strResult = dblAnswer.ToString("n")
+        lblFormatResult.Text = strResult
+    End Sub
+
+    Private Sub btnFixedPointFormat_Click(sender As Object, e As EventArgs) Handles btnFixedPointFormat.Click
+        Dim dblAnswer As Double
+        Dim strResult As String
+
+        dblAnswer = CDbl(txtUserInput.Text)
+
+
+        strResult = dblAnswer.ToString("f")
+        lblFormatResult.Text = strResult
+    End Sub
+
+    Private Sub btnExponentialFormat_Click(sender As Object, e As EventArgs) Handles btnExponentialFormat.Click
+        Dim dblAnswer As Double
+        Dim strResult As String
+
+        dblAnswer = CDbl(txtUserInput.Text)
+
+
+        strResult = dblAnswer.ToString("e")
+        lblFormatResult.Text = strResult
+    End Sub
+
+    Private Sub btnCurrencyFormat_Click(sender As Object, e As EventArgs) Handles btnCurrencyFormat.Click
+        Dim dblAnswer As Double
+        Dim strResult As String
+
+        dblAnswer = CDbl(txtUserInput.Text)
+
+
+        strResult = dblAnswer.ToString("c")
+        lblFormatResult.Text = strResult
+    End Sub
+
+    Private Sub btnPercentFormat_Click(sender As Object, e As EventArgs) Handles btnPercentFormat.Click
+        Dim dblAnswer As Double
+        Dim strResult As String
+
+        dblAnswer = CDbl(txtUserInput.Text)
+
+
+        strResult = dblAnswer.ToString("p")
+        lblFormatResult.Text = strResult
+    End Sub
+
+    Private Sub btnShortDate_Click(sender As Object, e As EventArgs) Handles btnShortDate.Click
+        Dim Answer As Date
+        Dim strResult As String
+
+        Answer = CDate(txtUserInput.Text)
+
+
+        strResult = Answer.ToString("d")
+        lblFormatResult.Text = strResult
+    End Sub
+
+    Private Sub btnLongDate_Click(sender As Object, e As EventArgs) Handles btnLongDate.Click
+        Dim Answer As Date
+        Dim strResult As String
+
+        Answer = CDate(txtUserInput.Text)
+
+
+        strResult = Answer.ToString("D")
+        lblFormatResult.Text = strResult
+    End Sub
+
+    Private Sub btnShortTime_Click(sender As Object, e As EventArgs) Handles btnShortTime.Click
+        Dim Answer As Date
+        Dim strResult As String
+
+        Answer = CDate(txtUserInput.Text)
+
+
+        strResult = Answer.ToString("t")
+        lblFormatResult.Text = strResult
+    End Sub
+
+    Private Sub btnLongTime_Click(sender As Object, e As EventArgs) Handles btnLongTime.Click
+        Dim Answer As Date
+        Dim strResult As String
+
+        Answer = CDate(txtUserInput.Text)
+
+
+        strResult = Answer.ToString("T")
+        lblFormatResult.Text = strResult
+    End Sub
+
+    Private Sub btnFullDateTime_Click(sender As Object, e As EventArgs) Handles btnFullDateTime.Click
+        Dim Answer As Date
+        Dim strResult As String
+
+        Answer = CDate(txtUserInput.Text)
+
+
+        strResult = Answer.ToString("F")
+        lblFormatResult.Text = strResult
+    End Sub
+End Class
